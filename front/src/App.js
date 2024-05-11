a490b34ec7ce5264f62b4e39d7b0fdb561696dbe
AndyForWork2 <wwwlolkeke@gmail.com>
2024-05-11 18:22:43 +0300

    lab 9

--- front/src/App.js
+++ front/src/App.js
@@ -3,17 +3,19 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import {createBrowserHistory} from "history";
 
-import NavigationBar from "./components/NavigationBar";
+import NavigationBarClass from "./components/NavigationBar";
 import Home from "./components/Home";
+import Login from "./components/Login";
 
 function App() {
     return (
         <div className="App">
             <BrowserRouter>
-                <NavigationBar />
+                <NavigationBarClass />
                 <div className="container-fluid">
                     <Routes>
                         <Route path="home" element={<Home />}/>
+                        <Route path="login" element={<Login/>} />
                     </Routes>
                 </div>
             </BrowserRouter>
